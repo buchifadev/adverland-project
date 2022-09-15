97f545fdc65a3f587dc5b624fe6ea0e455de176a
Favour Buchi <favobuchi@outlook.com>
2022-09-15 12:34:11 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import BigNumber from "bignumber.js";
+import { newKitFromWeb3 } from "@celo/contractkit";
+import adverlandAbi from "./contract/adverland.abi.json";
+import Web3 from "web3";
+
+
+const ERC20_DECIMALS = 18;
+const advlContract = "";
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
